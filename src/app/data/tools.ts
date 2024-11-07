fef3c4a0668678f5bfb9827175805bc10c25391b
Victor Manuel <mbvictorsantos@gmail.com>
2024-11-07 13:15:20 -0300

    feat: Add more tools

--- src/app/data/tools.ts
+++ src/app/data/tools.ts
@@ -16,67 +16,87 @@ export const categories: Category[] = [
       {
         title: 'Xeon E5',
         description: 'An old processor for servers.',
-        url: 'TODO',
+        url: '',
       },
       {
         title: 'X79 Motherboard',
         description: 'A not well known motherboard for a not well known processor.',
-        url: 'TODO'
+        url: ''
       },
       {
         title: 'RTX 4060 Ti',
         description: 'Just for gaming.',
-        url: 'TODO'
+        url: 'https://amzn.to/4fsUtMC'
       },
       {
-        title: 'SSD',
+        title: 'SSD SATA Kingston 480GB',
         description: '',
-        url: 'TODO'
+        url: 'https://amzn.to/4fijwlL'
       },
       {
-        title: 'HDD',
+        title: 'SSD M.2 KingSpec 512GB',
         description: '',
-        url: 'TODO'
+        url: 'https://amzn.to/3YFORaP'
       },
       {
-        title: 'Case',
+        title: 'HDD WD Blue 1TB',
         description: '',
-        url: 'TODO'
+        url: 'https://amzn.to/4eszZCU'
       },
+      // {
+      //   title: 'Case',
+      //   description: '',
+      //   url: ''
+      // },
       {
-        title: 'Fans',
-        description: '',
-        url: 'TODO'
+        title: '3x 120mm Rise Mode fans',
+        description: 'The Name of the Wind',
+        url: 'https://amzn.to/40x22h2'
       },
     ]
   },
   {
     name: 'Periphericals',
     items: [
+      // {
+      //   title: 'Keyboard',
+      //   description: 'Just for gaming.',
+      //   url: 'TODO'
+      // },
       {
-        title: 'Keyboard',
-        description: 'Just for gaming.',
-        url: 'TODO'
+        title: 'Mouse Redragon Cobra',
+        description: '7 buttons are great.',
+        url: 'https://amzn.to/40D5zuf'
+      },
+      // {
+      //   title: 'Microphone',
+      //   description: 'Just for gaming.',
+      //   url: 'TODO'
+      // },
+      // {
+      //   title: 'Headset',
+      //   description: 'Just for gaming.',
+      //   url: 'TODO'
+      // },
+      // {
+      //   title: 'Monitor',
+      //   description: 'Just for gaming.',
+      //   url: 'TODO'
+      // },
+      {
+        title: '8Bitdo Ultimate 2C Wireless Controller',
+        description: 'Black Myth: Wukong edition.',
+        url: 'https://amzn.to/4fbmCYK'
       },
       {
-        title: 'Redragon Kobra',
-        description: 'Just for gaming.',
-        url: 'TODO'
+        title: '8Bitdo Ultimate Controller',
+        description: 'For Nintendo Switch.',
+        url: 'https://amzn.to/3AwHxGA'
       },
       {
-        title: 'Microphone',
-        description: 'Just for gaming.',
-        url: 'TODO'
-      },
-      {
-        title: 'Headset',
-        description: 'Just for gaming.',
-        url: 'TODO'
-      },
-      {
-        title: 'Monitor',
-        description: 'Just for gaming.',
-        url: 'TODO'
+        title: 'Nintendo Switch OLED',
+        description: 'My favorite videogame.',
+        url: 'https://amzn.to/3CrvwCO'
       },
     ]
   },
