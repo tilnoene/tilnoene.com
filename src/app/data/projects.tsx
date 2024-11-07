14245d14ebb64dcf28e822c7be2ce8838355deeb
Victor Manuel <mbvictorsantos@gmail.com>
2024-11-07 14:20:45 -0300

    fix: Change descriptions, urls and icons of some projects

--- src/app/data/projects.tsx
+++ src/app/data/projects.tsx
@@ -12,13 +12,13 @@ import { PiPlanet } from 'react-icons/pi'
 import { GoPerson } from 'react-icons/go'
 import { PiCassetteTapeLight } from 'react-icons/pi'
 import { SlCalculator } from 'react-icons/sl'
-import { BsQuestionLg } from 'react-icons/bs'
 import { PiHandGrabbing } from 'react-icons/pi'
 import { FiCpu } from 'react-icons/fi'
 import { FaStethoscope } from 'react-icons/fa6'
 import { TbTruckDelivery } from 'react-icons/tb'
 import { TbDeviceGamepad } from 'react-icons/tb'
 import { TbMicrophone2 } from 'react-icons/tb'
+import { TbPokeball } from 'react-icons/tb'
 
 export type Project = {
   name: string
@@ -26,6 +26,7 @@ export type Project = {
   icon?: JSX.Element
   slug?: string
   sourceUrl: string
+  private?: boolean
 }
 
 export const featuredProjects: Project[] = [
@@ -45,8 +46,8 @@ export const featuredProjects: Project[] = [
   },
   {
     name: 'CP Notebook Template',
-    description: 'Automatic notebook generator for DSA.',
-    slug: '5+ forks',
+    description: 'Template generator for organized coding notes.',
+    slug: '5+ copies',
     icon: <PiNotebook size={32} />,
     sourceUrl: 'https://github.com/tilnoene/competitive-programming-template',
   },
@@ -61,13 +62,13 @@ export const projects: Project[] = [
   },
   {
     name: 'Celeste',
-    description: 'Assembly recreation of Celeste game.',
+    description: 'Recreation of the Celeste game in assembly language.',
     icon: <PiMountains size={32} />,
     sourceUrl: 'https://github.com/tilnoene/chocadeira',
   },
   {
     name: 'Repet',
-    description: 'Pet manager.',
+    description: 'App to manage pet vaccines and general care.',
     icon: <FaStethoscope size={32} />,
     sourceUrl: 'https://github.com/tilnoene/repet',
   },
@@ -75,92 +76,92 @@ export const projects: Project[] = [
     name: 'tilnoene.com',
     description: 'My personal website, also known as "this website".',
     icon: <GoPerson size={32} />,
-    sourceUrl: 'https://github.com/tilnoene/repet',
+    sourceUrl: 'https://github.com/tilnoene/tilnoene.com',
   },
   {
     name: 'ZeptoProcessor',
     description: 'A 16-bit processor created from scratch.',
     icon: <FiCpu size={32} />,
-    sourceUrl: 'https://github.com/tilnoene/repet',
+    sourceUrl: 'https://github.com/tilnoene/zeptoprocessador',
   },
   {
     name: 'Viniccius13 Facts',
-    description: 'Sudoku generator and solver.',
+    description: 'A website with funny quotes about Viniccius13.',
     icon: <PiQuotes size={32} />,
-    sourceUrl: 'https://github.com/tilnoene/sudoku',
+    sourceUrl: 'https://github.com/tilnoene/viniccius13facts-front',
   },
   {
     name: 'Mashup Builder',
-    description: 'Random mashup builder for codeforces.',
+    description: 'Selects random Codeforces problems by rating.',
     icon: <GoGear size={32} />,
     sourceUrl: 'https://github.com/tilnoene/chocadeira',
   },
   {
     name: 'PetGato',
-    description: 'Food delivery app.',
+    description: 'Blog for a veterinarian, sharing pet care tips.',
     icon: <IoPawOutline size={32} />,
-    sourceUrl: 'https://github.com/tilnoene/repet',
+    sourceUrl: 'https://petgato-git-develop-tilnoene.vercel.app/erro',
   },
 
   {
     name: 'UaiFood',
-    description: 'Food delivery app.',
+    description: 'Simplified version of a food delivery app.',
     icon: <TbTruckDelivery size={32} />,
-    sourceUrl: 'https://github.com/tilnoene/repet',
+    sourceUrl: 'https://github.com/tilnoene/uaifood',
   },
   {
     name: 'SpeedCalc',
-    description: 'Math game to solve expressions faster.',
+    description: 'Solve random math questions as fast as possible.',
     icon: <SlCalculator size={32} />,
-    sourceUrl: 'https://github.com/tilnoene/repet',
+    sourceUrl: 'https://speedcalc.vercel.app/',
   },
   {
     name: 'ChaTil',
     description: 'Real time chat using websocket.',
     icon: <PiChatText size={32} />,
-    sourceUrl: 'https://github.com/tilnoene/repet',
+    sourceUrl: 'https://github.com/tilnoene/chat',
   },
   {
     name: 'Gauntlet',
-    description: 'Food delivery app.',
+    description: 'RC car controlled by glove movement.',
     icon: <PiHandGrabbing size={32} />,
-    sourceUrl: 'https://github.com/tilnoene/repet',
+    sourceUrl: 'https://github.com/tilnoene/manopla',
   },
   {
     name: 'Solar System',
     description: 'Solar system made using Three.js.',
     icon: <PiPlanet size={32} />,
-    sourceUrl: 'https://github.com/tilnoene/repet',
+    sourceUrl: 'https://solar-system-treejs.vercel.app/',
   },
   {
     name: 'CP Badges',
     description: 'Competitive programming badges for GitHub.',
     icon: <LiaMedalSolid size={32} />,
-    sourceUrl: 'https://github.com/tilnoene/repet',
+    sourceUrl: 'https://github.com/kehsihba19/CP-Badges',
   },
   {
     name: 'Way of the Exploding Fist',
-    description: 'Assembly recreation of the game.',
+    description: 'Recreation of this game in assembly language.',
     icon: <TbDeviceGamepad size={32} />,
-    sourceUrl: 'https://github.com/tilnoene/repet',
+    sourceUrl: 'https://github.com/marcelomamorim/RISC-V-assembly-game',
   },
 
   {
     name: 'Guess the Lyrics',
-    description: 'Food delivery app.',
+    description: 'Fill in the random missing words of a song.',
     icon: <TbMicrophone2 size={32} />,
-    sourceUrl: 'https://github.com/tilnoene/repet',
+    sourceUrl: 'https://github.com/tilnoene/guess-the-lyrics',
   },
   {
     name: 'Pokemon Quiz',
-    description: 'Food delivery app.',
-    icon: <BsQuestionLg size={32} />,
-    sourceUrl: 'https://github.com/tilnoene/repet',
+    description: "Who's That Pokémon? Guess it from a silhouette.",
+    icon: <TbPokeball size={32} />,
+    sourceUrl: 'https://github.com/tilnoene/pokemon-quiz',
   },
   {
     name: 'Cassette Tape',
-    description: 'Food delivery app.',
+    description: 'A basic web music player built without frameworks.',
     icon: <PiCassetteTapeLight size={32} />,
-    sourceUrl: 'https://github.com/tilnoene/repet',
+    sourceUrl: 'https://github.com/tilnoene/cassette-tape',
   },
 ]
