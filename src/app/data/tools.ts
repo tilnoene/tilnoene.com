45851db65f8353cd052788df7a4db7d97fe9ac3d
Victor Manuel <mbvictorsantos@gmail.com>
2024-11-13 15:26:34 -0300

    feat: Added some tools that I use

--- src/app/data/tools.ts
+++ src/app/data/tools.ts
@@ -1,12 +1,12 @@
 export type Item = {
-  title: string,
-  description: string,
-  url: string,
+  title: string
+  description?: string
+  url?: string
 }
 
 export type Category = {
-  name: string;
-  items: Item[],
+  name: string
+  items: Item[]
 }
 
 export const categories: Category[] = [
@@ -14,90 +14,93 @@ export const categories: Category[] = [
     name: 'Computer',
     items: [
       {
-        title: 'Xeon E5',
+        title: 'Xeon E5-2630 V2',
         description: 'An old processor for servers.',
-        url: '',
       },
       {
-        title: 'X79 Motherboard',
-        description: 'A not well known motherboard for a not well known processor.',
-        url: ''
+        title: 'Atermiter X79T',
+        description:
+          'A not well known motherboard for a not well known processor.',
       },
       {
-        title: 'RTX 4060 Ti',
+        title: '16GB DDR3 1333 MHz (4x 4GB)',
+        description: 'No brand.',
+      },
+      {
+        title: 'RTX 4060 Ti Gigabyte',
         description: 'Just for gaming.',
-        url: 'https://amzn.to/4fsUtMC'
+        url: 'https://amzn.to/4fsUtMC',
       },
       {
         title: 'SSD SATA Kingston 480GB',
-        description: '',
-        url: 'https://amzn.to/4fijwlL'
+        url: 'https://amzn.to/4fijwlL',
       },
       {
         title: 'SSD M.2 KingSpec 512GB',
-        description: '',
-        url: 'https://amzn.to/3YFORaP'
+        url: 'https://amzn.to/3YFORaP',
       },
       {
         title: 'HDD WD Blue 1TB',
-        description: '',
-        url: 'https://amzn.to/4eszZCU'
+        url: 'https://amzn.to/4eszZCU',
       },
-      // {
-      //   title: 'Case',
-      //   description: '',
-      //   url: ''
-      // },
+      {
+        title: 'Pichau Komor Led RGB',
+        description: 'A beautiful and spacious case.',
+        url: 'https://www.pichau.com.br/gabinete-gamer-pichau-komor-led-rgb-lateral-em-vidro-temp-pgk-01-rgb',
+      },
       {
         title: '3x 120mm Rise Mode fans',
-        description: 'The Name of the Wind',
-        url: 'https://amzn.to/40x22h2'
+        description: 'The Name of the Wind.',
+        url: 'https://amzn.to/40x22h2',
       },
-    ]
+    ],
   },
   {
     name: 'Periphericals',
     items: [
-      // {
-      //   title: 'Keyboard',
-      //   description: 'Just for gaming.',
-      //   url: 'TODO'
-      // },
       {
-        title: 'Mouse Redragon Cobra',
-        description: '7 buttons are great.',
-        url: 'https://amzn.to/40D5zuf'
+        title: 'HyperX Alloy Origins Core (blue switch)',
+        description: 'An absolutely amazing mechanical keyboard with ABNT2.',
+        url: 'https://amzn.to/40HpdFz',
       },
-      // {
-      //   title: 'Microphone',
-      //   description: 'Just for gaming.',
-      //   url: 'TODO'
-      // },
-      // {
-      //   title: 'Headset',
-      //   description: 'Just for gaming.',
-      //   url: 'TODO'
-      // },
+      {
+        title: 'Redragon Cobra Mouse',
+        description: 'The 7 buttons are great.',
+        url: 'https://amzn.to/40D5zuf',
+      },
+      {
+        title: 'Husky Howl',
+        description: 'A basic condenser microphone.',
+        url: 'https://www.kabum.com.br/produto/113993/microfone-gamer-husky-gaming-howl-podcast-preto-usb-condensador-hgmm001',
+      },
+      {
+        title: 'Havit H2002D Headset',
+        description: 'The plug-in mic is a game-changer.',
+        url: 'https://amzn.to/3AO8x4r',
+      },
       // {
       //   title: 'Monitor',
       //   description: 'Just for gaming.',
       //   url: 'TODO'
       // },
       {
+        title: 'Generic Mousepad 900x350x3mm',
+      },
+      {
         title: '8Bitdo Ultimate 2C Wireless Controller',
         description: 'Black Myth: Wukong edition.',
-        url: 'https://amzn.to/4fbmCYK'
+        url: 'https://amzn.to/4fbmCYK',
       },
       {
         title: '8Bitdo Ultimate Controller',
         description: 'For Nintendo Switch.',
-        url: 'https://amzn.to/3AwHxGA'
+        url: 'https://amzn.to/3AwHxGA',
       },
       {
         title: 'Nintendo Switch OLED',
         description: 'My favorite videogame.',
-        url: 'https://amzn.to/3CrvwCO'
+        url: 'https://amzn.to/3CrvwCO',
       },
-    ]
+    ],
   },
 ]
--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -1,10 +1,17 @@
+import Head from 'next/head'
+
 import GradientTitle from '@/app/components/GradientTitle'
 
 import { categories, Category, Item } from '@/app/data/tools'
 
-export default function Projects() {
+export default function Tools() {
   return (
     <div className="flex-1">
+      <Head>
+        <title>Tools | Victor Santos</title>
+        <meta property="og:title" content="Tools | Victor Santos" key="title" />
+      </Head>
+
       <div>
         <GradientTitle className="from-orange to-yellow mb-2">
           Tools, Apps & Gear
@@ -18,21 +25,31 @@ export default function Projects() {
         </p>
       </div>
 
-      <div className="mt-10 flex flex-col gap-6">
+      <div className="mt-10 flex flex-col gap-7">
         {categories.map((category: Category) => (
           <div key={category.name}>
             <h2>{category.name}</h2>
 
-            <ul>
+            <ul className="flex flex-col gap-2 mt-1.5">
               {category.items.map((item: Item) => (
                 <li key={item.title}>
                   <p>
-                    <a href={item.url} target="_blank" rel="noreferrer">
-                      {item.title}
-                    </a>{' '}
-                    {item.description !== '' && (
-                      <span>- {item.description}</span>
+                    <span className="mr-1 ml-2"> • </span>
+
+                    {item.url ? (
+                      <a
+                        href={item.url}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="hover:opacity-75"
+                      >
+                        {item.title}
+                      </a>
+                    ) : (
+                      <span className="text-primary">{item.title}</span>
                     )}
+
+                    {item.description && <span> - {item.description}</span>}
                   </p>
                 </li>
               ))}
