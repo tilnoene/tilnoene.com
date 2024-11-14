2e42dd006d662fb5dbad7544f998307a27c63eaa
Victor Manuel <mbvictorsantos@gmail.com>
2024-11-14 18:44:56 -0300

    fix: Change resume file

--- src/app/data/about.ts
+++ src/app/data/about.ts
@@ -1,27 +1,27 @@
 export type Job = {
-  title: string,
-  type: string,
-  company: string,
-  companyUrl: string,
-  startDate: string,
-  endDate: string,
-  location: string,
-};
+  title: string
+  type: string
+  company: string
+  companyUrl: string
+  startDate: string
+  endDate: string
+  location: string
+}
 
 export type Education = {
-  title: string,
-  universityName: string,
-  universityUrl: string,
-  startDate: string,
-  endDate: string,
-  location: string,
+  title: string
+  universityName: string
+  universityUrl: string
+  startDate: string
+  endDate: string
+  location: string
 }
 
 export type Award = {
-  title: string,
-  competition: string,
-  competitionUrl: string,
-  date: string,
+  title: string
+  competition: string
+  competitionUrl: string
+  date: string
 }
 
 export const jobs: Job[] = [
@@ -44,14 +44,14 @@ export const jobs: Job[] = [
     location: 'Brasília, Brazil',
   },
   {
-    title: 'Software Developer',
+    title: 'Web Developer',
     type: 'Part Time',
     company: 'CJR',
     companyUrl: 'https://www.cjr.org.br/',
     startDate: '2021-01-01',
     endDate: '2022-06-01',
     location: 'Brasília, Brazil',
-  }
+  },
 ]
 
 export const education: Education[] = [
@@ -62,26 +62,26 @@ export const education: Education[] = [
     startDate: '2020-06-01',
     endDate: '2025-06-01',
     location: 'Brasília, Brazil',
-  }
+  },
 ]
 
 export const awards: Award[] = [
   {
-    title: "Silver Medal",
-    competition: "Brazilian Olympiad in Informatics (OBI)",
-    competitionUrl: "https://olimpiada.ic.unicamp.br/",
-    date: "2021"
+    title: 'Silver Medal',
+    competition: 'Brazilian Olympiad in Informatics (OBI)',
+    competitionUrl: 'https://olimpiada.ic.unicamp.br/',
+    date: '2021',
   },
   {
-    title: "Participation on ICPC Brazil Subregionals",
-    competition: "International Collegiate Programming Contest (ICPC)",
-    competitionUrl: "https://icpc.global",
-    date: "2020 – 2024",
+    title: 'Participation on ICPC Brazil Subregionals',
+    competition: 'International Collegiate Programming Contest (ICPC)',
+    competitionUrl: 'https://icpc.global',
+    date: '2020 – 2024',
   },
   {
-    title: "Bronze Medal",
-    competition: "Brazilian Mathematics Olympiad (OBMEP)",
-    competitionUrl: "http://obmep.org.br/",
-    date: "2019"
+    title: 'Bronze Medal',
+    competition: 'Brazilian Mathematics Olympiad (OBMEP)',
+    competitionUrl: 'http://obmep.org.br/',
+    date: '2019',
   },
 ]
